3d6186bb35e1a28e18e33184021e43304fd22ddf
gdmec07150732 <orlov553151076@163.com>
2017-01-01 20:13:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/orlov/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package com.example.orlov.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by orlov on 2016/10/30.
+ */
+public class IntentDemo3 extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
